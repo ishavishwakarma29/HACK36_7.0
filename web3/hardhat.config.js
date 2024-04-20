ce176b5d190d4f20aa235366aea1c595ef8e4176
akhil1g <akhil.gupta.12817@gmail.com>
2024-04-20 07:35:41 +0530

    minorChange

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,35 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-
-const SEPOLIA_RPC_URL=process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY=process.env.PRIVATE_KEY
+const SEPOLIA_RPC_URL=process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY=process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.7",
-  defaultNetwork:"hardhat",
-  networks:{
-    sepolia:{
-      url:SEPOLIA_RPC_URL,
-      accounts:[PRIVATE_KEY],
-      chainId:11155111,
+  solidity: "0.8.4",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
     }
   },
 };
